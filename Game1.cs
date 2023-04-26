f1e4fa3877c95cc884120625e12fb30734e09153
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-04-26 11:03:39 -0400

    adding back backgroud

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 
 namespace Lesson_6___Summative
@@ -10,11 +11,18 @@ namespace Lesson_6___Summative
         List<Texture2D> dinoTextures;
         Texture2D dinoSpritesheet;
 
-        int dinoIndex;
+        Texture2D groundTexture, treesAndBushesTexture, distantTreesTexture, bushesTexture, hill1Texture, hill2Texture;
+        Texture2D hugeCloudsTexture, cloudsTexture, distantClouds1Texture, distantClouds2Texture, backgroundTexture;
+
+        Rectangle bgRect;
+
+        double dinoIndex;
 
         Rectangle dinoRect;
 
-        MouseState mouseState, prevMouseState;
+        MouseState mouseState;
+
+        Vector2 dinoSpeed;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -30,13 +38,17 @@ namespace Lesson_6___Summative
         {
             // TODO: Add your initialization logic here
 
-            _graphics.PreferredBackBufferWidth = 800;
-            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
             _graphics.ApplyChanges();
 
-            dinoRect = new Rectangle(300, 50, 100, 100);
+            dinoRect = new Rectangle(-100, 600, 48, 48);
             dinoTextures = new List<Texture2D>();
             dinoIndex = 0;
+
+            dinoSpeed = new Vector2(1, 0);
+
+            bgRect = new Rectangle(0, 0, 1280, 720);
 
             base.Initialize();
         }
@@ -49,42 +61,56 @@ namespace Lesson_6___Summative
 
             dinoSpritesheet = Content.Load<Texture2D>("dino_sprites");
 
+            groundTexture = Content.Load<Texture2D>("_01_ground");
+            treesAndBushesTexture = Content.Load<Texture2D>("_02_trees_and_bushes");
+            distantTreesTexture = Content.Load<Texture2D>("_03_distant_trees");
+            bushesTexture = Content.Load<Texture2D>("_04_bushes");
+            hill1Texture = Content.Load<Texture2D>("_05_hill1");
+            hill2Texture = Content.Load<Texture2D>("_06_hill2");
+            hugeCloudsTexture = Content.Load<Texture2D>("_07_huge_clouds");
+            cloudsTexture = Content.Load<Texture2D>("_08_clouds");
+            distantClouds1Texture = Content.Load<Texture2D>("_09_distant_clouds1");
+            distantClouds2Texture = Content.Load<Texture2D>("_10_distant_clouds");
+            backgroundTexture = Content.Load<Texture2D>("_11_background");
+
+
+
             Texture2D cropTexture;
             Rectangle sourceRect;
 
             int width = dinoSpritesheet.Width / 24;
-            int height = dinoSpritesheet.Height / 1;
+            int height = dinoSpritesheet.Height;
 
-            for (int y = 1; y < 1; y++)
+           
+            for (int x = 5; x < 10; x++)
             {
-                for (int x = 1; x < 24; x++)
-                {
-                    sourceRect = new Rectangle(x * width, y * height, width, height);
-                    cropTexture = new Texture2D(GraphicsDevice, width, height);
+                sourceRect = new Rectangle(x * width, 0, width, height);
+                cropTexture = new Texture2D(GraphicsDevice, width, height);
 
-                    Color[] data = new Color[width * height];
-                    dinoSpritesheet.GetData(0, sourceRect, data, 0, data.Length);
+                Color[] data = new Color[width * height];
+                dinoSpritesheet.GetData(0, sourceRect, data, 0, data.Length);
 
-                    cropTexture.SetData(data);
+                cropTexture.SetData(data);
 
-                    if (dinoTextures.Count < 24)
-                        dinoTextures.Add(cropTexture);
-                }
+                dinoTextures.Add(cropTexture);
             }
+            
         }
 
         protected override void Update(GameTime gameTime)
         {
+            mouseState = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
 
-            if (mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released)
-                dinoIndex -= 1;
+            dinoIndex += 0.1;
 
-            else if (mouseState.RightButton == ButtonState.Pressed && prevMouseState.RightButton == ButtonState.Released)
-                dinoIndex += 1;
+            if (dinoIndex >= dinoTextures.Count - 0.5)
+                dinoIndex = 0;
+
+            dinoRect.X += (int)dinoSpeed.X;
 
             base.Update(gameTime);
         }
@@ -95,7 +121,12 @@ namespace Lesson_6___Summative
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(dinoTextures[dinoIndex], dinoRect, Color.White);
+
+            _spriteBatch.Draw(groundTexture, bgRect, Color.White);
+            //continue on rest of background...VV
+
+
+            _spriteBatch.Draw(dinoTextures[(int)Math.Round(dinoIndex)], dinoRect, Color.White);
             _spriteBatch.End();
 
             base.Draw(gameTime);
